3e46fd6ea67f9dff75ca57be04cc8bbc8d0cc30d
Suraj Kumar <sr6447868@gmail.com>
2025-07-31 20:57:04 +0530

    Fix: downgraded Next.js for Windows path bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Footer from '@/components/Footer';
+// import Footer from '@/components/Footer';
 
 import { FaInstagram, FaFacebookF, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
